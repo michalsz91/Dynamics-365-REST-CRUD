34d9a34ff75861c1b5baf2595ea8c5f2fba918a8
Michał Szcześniak <szczesniakm91@gmail.com>
2020-09-19 13:20:39 +0200

    initial commit

---
+++ Dynamics-365-REST-CRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dynamics_365_REST_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
