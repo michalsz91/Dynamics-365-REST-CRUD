cf101564e5bb8ab34038a25c89181f87fa585591
Michał Szcześniak <szczesniakm91@gmail.com>
2020-09-19 16:14:02 +0200

    working authentication and getting data

--- Dynamics-365-REST-CRUD/Program.cs
+++ Dynamics-365-REST-CRUD/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Dynamics_365_REST_CRUD
 {
     class Program
     {
+
+        private static HttpClient httpClient = new HttpClient();
+
+        private static string serviceUrl = "https://msz12.crm4.dynamics.com";
+
+        private static string clientId = "";
+
+        private static string secret = "";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            AuthenticationContext authContext = new AuthenticationContext("https://login.microsoftonline.com/tenantid");
+            ClientCredential credential = new ClientCredential(clientId, secret);
+
+            AuthenticationResult result = authContext.AcquireTokenAsync(serviceUrl, credential).Result;
+
+            string accessToken = result.AccessToken;
+
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", accessToken);
+
+            HttpResponseMessage respone = httpClient.GetAsync("https://msz12.api.crm4.dynamics.com/api/data/v9.1/accounts").Result;
+            string data = respone.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(data);
+
         }
     }
 }
